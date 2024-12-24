345508c61b0b5c232ad8ddcdde84245f6f77d4dd
Ole Jørgensen <ole.i.jorgensen+github@outlook.dk>
2024-12-24 10:47:13 +0100

    .

--- mrp/Displacement.sql
+++ mrp/Displacement.sql
@@ -19,8 +19,26 @@ CREATE TABLE if NOT EXISTS Displacement (
 ,   DisplacementKind_Id     INTEGER NOT NULL                    
 ,   Qty                     DECIMAL(38,9)
 ,   EffectiveDate           DATE NOT NULL
+,   Salesorder_Id           INTEGER NULL -- date(EffectiveDate,'unixepoch'), unixepoch('now', 'utc', '+5 MONTHS')
 ,   CONSTRAINT Displacement_FK01 FOREIGN KEY (Item_Id) REFERENCES Item(Id)
 ,   CONSTRAINT Displacement_FK02 FOREIGN KEY (DisplacementKind_Id) REFERENCES DisplacementKind(Id)
+,   CONSTRAINT Displacement_FK03 FOREIGN KEY (Salesorder_Id) REFERENCES Salesorder(Id)
 );
 
-INSERT INTO Displacement(Item_Id, DisplacementKind_Id, Qty, EffectiveDate) VALUES(1, 4, -19288.0, strftime('%s', 'now', 'utc', '5 MONTHS'));
+INSERT INTO Displacement(Item_Id, DisplacementKind_Id, Qty, EffectiveDate, Salesorder_Id) 
+    VALUES(1, 1, -100, unixepoch('now', 'utc', '+5 MONTHS'), 1)
+    ,   (1, 1, -200, unixepoch('now', 'utc', '+6 MONTHS'), 2)
+    ,   (1, 1, -1350, unixepoch('now', 'utc', '+7 MONTHS'), 3)
+    ,   (9, 2, 1350, unixepoch('now', 'utc', '+7 MONTHS'), NULL)
+    ;
+
+select
+    Displacement.Id
+,   DisplacementKind.DisplacementKind
+,   date(Displacement.EffectiveDate,'unixepoch') as EffectiveDate
+,   SalesOrder.Id
+,   SalesOrder.SalesOrderNumber
+,   date(SalesOrder.ShipDate, 'unixepoch') as Shipdate
+from Displacement
+inner join DisplacementKind on DisplacementKind.Id = Displacement.DisplacementKind_Id
+left join SalesOrder on SalesOrder.Id = Displacement.Salesorder_Id
--- mrp/Salesorder.sql
+++ mrp/Salesorder.sql
@@ -4,14 +4,14 @@
 CREATE TABLE if NOT EXISTS Salesorder (
     Id                      INTEGER NOT NULL PRIMARY KEY
 ,   SalesOrderNumber        VARCHAR(16) NOT NULL
-,   Displacement_Id         INTEGER NOT NULL
+,   Item_Id                 INTEGER NOT NULL
+,   Qty                     DECIMAL(38,9)
 ,   ShipDate                DATE NOT NULL
---,   CONSTRAINT Salesorder_FK01 FOREIGN KEY (Item_Id) REFERENCES Item(Id)
-,   CONSTRAINT Salesorder_FK02 FOREIGN KEY (Displacement_Id) REFERENCES Displacement(Id)
+,   CONSTRAINT Salesorder_FK01 FOREIGN KEY (Item_Id) REFERENCES Item(Id)
 );
 
--- INSERT INTO Salesorder (Item_id,Qty,ShipDate) 
---     values(1,100,strftime('%s', 'now', 'utc', '5 MONTHS') )
---     ,(1,100,strftime('%s', 'now', 'utc', '6 MONTHS') )
---     ,(1,100,strftime('%s', 'now', 'utc', '7 MONTHS') )
--- ;
+INSERT INTO Salesorder (SalesOrderNumber,Item_Id,Qty,ShipDate) 
+    VALUES('F023931',   1, 100,  unixepoch('now', 'utc', '+5 MONTHS') )--strftime('%s', 'now', 'utc', '5 MONTHS') )
+    ,     ('F001331-0', 1, 200,  unixepoch('now', 'utc', '+6 MONTHS') )
+    ,     ('F099222',   1, 1350, unixepoch('now', 'utc', '+7 MONTHS') )
+;
