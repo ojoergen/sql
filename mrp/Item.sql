a63a5a7bcfd689bde7009ef5b596bb223a7ecb6a
Ole Jørgensen <ole.i.jorgensen+github@outlook.dk>
2025-04-18 10:00:45 +0200

    .

--- mrp/Item.sql
+++ mrp/Item.sql
@@ -1,9 +1,19 @@
 --
 -- ItemMaster
 --
+CREATE TABLE IF NOT EXISTS ItemSource (
+    Id                  INTEGER NOT NULL PRIMARY KEY
+,   Source              VARCHAR(3) NOT NULL
+);
+
+INSERT INTO ItemSource (Source)
+VALUES ('FG'),('SRC'),('ASS');
+
 CREATE TABLE IF NOT EXISTS Item (
     Id                  INTEGER NOT NULL PRIMARY KEY
 ,   Name                VARCHAR(30) NOT NULL
+,   SourceId            INTEGER NOT NULL
+,   CONSTRAINT Item_FK01 FOREIGN KEY(SourceId) REFERENCES ItemSource(SourceId)
 );
 -- INSERT INTO Item(Name) values('Item 1'),('Item 2'),('Item 3'),('Item 4'),('Item 5'),('Item 6'),('Item 7'),('Item 8'),('Item 9'),('Item 10');
 
@@ -26,4 +36,4 @@ CREATE TABLE IF NOT EXISTS Item_Stock (
 ,   CONSTRAINT Item_Stock_UK01 UNIQUE (Item_Id, Location_Id)
 );
 
-INSERT into Item_Stock (Item_Id,Location_Id,Qty) values(1,1,323.292837378),(1,2,3239383.22937378);
+--INSERT into Item_Stock (Item_Id,Location_Id,Qty) values(1,1,323.292837378),(1,2,3239383.22937378);
